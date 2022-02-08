612b9cb68fa93fca8c33c94a8adebf05942ed817
grandangelo <grandangelo@zoho.com>
2022-02-08 15:20:37 +0100

    Progress.

--- Get-Programming-With-F-Sharp-Capstones/Capstone 1/Program.fs
+++ Get-Programming-With-F-Sharp-Capstones/Capstone 1/Program.fs
@@ -1,20 +1,50 @@
-﻿open System
+﻿namespace Capstone1
 
+open System
 
-let waitForInput () = 
-    Console.ReadLine()
+type Destination =
+    | Home
+    | Office
+    | Stadium
+    | GasStation
 
+type PetrolUnit = int
 
-let rec manageInput () =
-    printfn "Insert command"
-    match waitForInput() with
-    | "q" -> 0
-    | _ -> printfn "Unsupported command"
-           manageInput()
+type TravelStage = {
+    Destination : Destination
+    PetrolCost : PetrolUnit
+}
 
+module main =
+    let stages = [
+        { Destination = Home; PetrolCost = -25 }
+        { Destination = Office; PetrolCost = -50 }
+        { Destination = Stadium; PetrolCost = -25 }
+        { Destination = GasStation; PetrolCost = 40 }
+    ]
 
-[<EntryPoint>]
-let main args =
-    printfn "Get programming with F# - Capstone 1"
-    printfn "q for quit"
-    manageInput()
+    let waitForInput () = 
+        Console.ReadLine()
+
+    let performTravel petrolUnit travelStage =
+        let petrolAfterTravel = petrolUnit + travelStage.PetrolCost
+        printfn "Traveling to %A. Petrol after travel: %i" travelStage.Destination petrolAfterTravel
+        petrolAfterTravel
+
+    let rec manageInput petrolUnit =
+        printfn "Current petrol: %A" petrolUnit
+        printfn "Insert destination"
+        printfn "q: quit - 1: Home - 2 Office - 3 Stadium - 4 GasStation"
+        match waitForInput() with
+        | "q" -> 0
+        | "1" | "2" | "3" | "4" -> // Travel
+                                   // Write remaining fuel
+                                   manageInput petrolUnit
+        | _ -> printfn "Unsupported command"
+               manageInput petrolUnit
+
+    [<EntryPoint>]
+    let main args =
+        printfn "Get programming with F# - Capstone 1"
+        printfn "q for quit"
+        manageInput 100
